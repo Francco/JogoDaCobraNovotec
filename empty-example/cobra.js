ca18307f1c5a969451cd3d491b09d5618afad646
Laboratório 02B Fatec <Lab02b@fatec.local>
2019-11-20 17:52:55 -0200

    comida

--- empty-example/cobra.js
+++ empty-example/cobra.js
@@ -3,7 +3,7 @@ function cobra(){
     this.y = 0;
     this.xvelocidade = 1;
     this.yvelocidade = 0;
-    
+
     this.update = function(){
         this.x += this.xvelocidade*tamanho;
         this.y += this.yvelocidade*tamanho;
@@ -11,6 +11,12 @@ function cobra(){
 
     this.show = function(){
         fill(255);
-        rect(this.x, this.y, tamanho, tamanho);
+        rect(this.x,this.y,tamanho,tamanho);
     }
+
+    this.dir = function(x,y){
+        this.xvelocidade = x;
+        this.yvelocidade = y;
+    }
+
 }
